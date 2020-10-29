16154f197c4649b4d36eae634f6e01136871af0d
Dylan McFarlin <dylanmcfarlin@Dylans-MacBook-Pro.local>
2020-10-29 16:15:23 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiniChallenge_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            
+            Console.WriteLine("What time did you wake up?");
+            string userWakeUp = Console.ReadLine();
+
+            Console.WriteLine(userName + " woke up today at " + userWakeUp);
+        }
+    }
+}
